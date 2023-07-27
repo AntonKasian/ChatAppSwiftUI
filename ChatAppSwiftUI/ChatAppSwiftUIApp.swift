3d1e117870cd3da77aa463621840cf992e0bb508
Anton Kasian <34170533+AntonKasian@users.noreply.github.com>
2023-07-27 10:37:27 +0400

    Initial commit

---
+++ ChatAppSwiftUI/ChatAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppSwiftUIApp.swift
+//  ChatAppSwiftUI
+//
+//  Created by Anton on 27.07.23.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
